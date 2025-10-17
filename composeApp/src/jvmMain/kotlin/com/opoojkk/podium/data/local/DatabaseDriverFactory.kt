c3f57235cf4546b33eea47ceef969a90374cd2f8
opoojkk <33021407+opoojkk@users.noreply.github.com>
2025-10-17 21:53:11 +0800

    refactor: improve database driver creation logic in DatabaseDriverFactory.kt

--- composeApp/src/jvmMain/kotlin/com/opoojkk/podium/data/local/DatabaseDriverFactory.kt
+++ composeApp/src/jvmMain/kotlin/com/opoojkk/podium/data/local/DatabaseDriverFactory.kt
@@ -4,7 +4,13 @@ import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.opoojkk.podium.db.PodcastDatabase
 
 actual class DatabaseDriverFactory {
-    actual fun createDriver() = JdbcSqliteDriver("jdbc:sqlite:podium.db").also { driver ->
-        runCatching { PodcastDatabase.Schema.create(driver) }
+    actual fun createDriver() = run {
+        val databasePath = "podium.db"
+        val databaseFileExisted = java.io.File(databasePath).exists()
+        val driver = JdbcSqliteDriver("jdbc:sqlite:$databasePath")
+        if (!databaseFileExisted) {
+            PodcastDatabase.Schema.create(driver)
+        }
+        driver
     }
 }
